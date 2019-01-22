f8103b4a2a2f6cdcc66165937f0fd5958bb9c08a
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-22 04:13:06 +0000

    updated locations migration attributes

--- db/migrate/002_create_locations.rb
+++ db/migrate/002_create_locations.rb
@@ -1,7 +1,10 @@
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
-
+      t.string :name
+      t.integer :price
+      t.integer :activity_rating
+      t.integer :excitement_rating
     end
   end
 end
