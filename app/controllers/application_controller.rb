2438b725609714a681c09898a304c07613d7de76
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-22 04:27:08 +0000

    added app controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  
+  def in_session?
+    !!session[:user_id]
+  end
+
+  def current_user
+    @user = User.find(session[:user_id])
+  end
 end
