41f692f00b6ee2ffe17e7726a08b0018070a635a
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-22 04:15:58 +0000

    updated trip migration attributes

--- db/migrate/003_create_trips.rb
+++ db/migrate/003_create_trips.rb
@@ -1,7 +1,9 @@
 class CreateTrips < ActiveRecord::Migration
   def change
     create_table :trips do |t|
-
+      t.integer :user_id
+      t.integer :location_id
+      t.integer :user_rating
     end
   end
 end
