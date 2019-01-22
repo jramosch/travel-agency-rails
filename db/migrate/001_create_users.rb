128e46ce869964b3bd9293c6cb3c711d7fb6c572
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-22 04:09:32 +0000

    updated users migration

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -3,7 +3,9 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-      
+      t.integer :travel_credits
+      t.string :mood
+      t.boolean :admin, default: false, null: false
     end
   end
 end
