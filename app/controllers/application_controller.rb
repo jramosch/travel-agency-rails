d6d5d0d076c99cb9c159765034e0404730733fb2
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-22 04:45:11 +0000

    added home route for app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   helper_method :current_user
-  
+
+  def home
+    if in_session?
+      @user = User.find(session[:user_id])
+      redirect_to "/users/#{@user.id}"
+    end
+  end
+
   def in_session?
     !!session[:user_id]
   end
