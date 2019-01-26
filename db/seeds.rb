5caa905e025fe9834f95795c1735733240b00d14
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-26 01:33:53 -0500

    adding user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+DATA = {
+  :user_keys =>
+    ["name", "ticket_credits", "energy", "fun", "password"]
+  :users => [
+    ["Kratos", 1030, 5, 2, "password"],
+    ["Arkam Knight", 920, 3, 3, "password"],
+    ["Bayonetta", 666, 1, 5, "password"]
+  ],
+  :location_leys =>
+  []
+}
