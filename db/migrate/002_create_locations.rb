ae8f136769760e4b00fc4d437f8457899e77eda7
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-26 01:40:53 -0500

    added location attribute

--- db/migrate/002_create_locations.rb
+++ db/migrate/002_create_locations.rb
@@ -2,6 +2,7 @@ class CreateLocations < ActiveRecord::Migration[5.2]
   def change
     create_table :locations do |t|
       t.string :name
+      t.text :description
       t.integer :price
       t.integer :activity_rating
       t.integer :excitement_rating
