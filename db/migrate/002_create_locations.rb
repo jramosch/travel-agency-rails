7ebac54b1b09036f20a4d8b31c51c3f5c247b11d
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-26 13:27:18 -0500

    updated user_id attribute

--- db/migrate/002_create_locations.rb
+++ db/migrate/002_create_locations.rb
@@ -6,6 +6,7 @@ class CreateLocations < ActiveRecord::Migration[5.2]
       t.integer :price
       t.integer :activity_rating
       t.integer :excitement_rating
+      t.integer :user_id
     end
   end
 end
