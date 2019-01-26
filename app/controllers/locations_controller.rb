a29145234dcf6aba00f4df74c134241a5b8a939e
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-26 03:44:57 -0500

    added create and index methods

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,6 +3,16 @@ class LocationsController < ApplicationController
     @location = Location.new
   end
 
+  def create
+    @location = Location.create(location_params)
+    return redirect_to '/locations/new' unless @location.save
+    redirect_to "/locations/#{@location.id}"
+  end
+
+  def index
+    @locations = Location.all
+  end
+
   private
   def location_params
     params.require(:location).permit(:name, :description, :price, :activity_rating, :excitement_rating)
