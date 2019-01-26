51fdf56ed6bcbc3acc8e10432a73a860fc11937f
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-25 22:56:00 -0500

    set user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
   before_action :require_login, only: [:show]
+
   def new
     @user = User.new
   end
 
   def create
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :travel_credits, :mood, :admin, :password, :password_confirmation)
   end
 end
