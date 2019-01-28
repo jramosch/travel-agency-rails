00a8c5621e8bf15454bda7a6a33e3e94b03f96f9
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-27 22:10:20 -0500

    updated take_trip method with if-else

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,9 +3,13 @@ class Trip < ActiveRecord::Base
   belongs_to :trip_location, class_name: 'Location', foreign_key: :location_id
 
   def take_trip
-    
-    if current_user.travel_credits < self.location.price .energy < self.location.energy_rating && current_user.fun
-
+    location = self.trip_location
+    if current_user.travel_credits < location.price && current_user.energy < location.activity_rating && current_user.fun < location.excitement_rating
+      "Sorry, you don't have enough credits and maybe you'd like something more relaxing & calmer."
+    elsif current_user.energy < location.activity_rating && current_user.fun < location.excitement_rating
+      "Sorry, maybe you want something a little more relaxing & calmer."
+    elsif current_user.fun < location.excitement_rating
+      "Sorry, maybe you want something a little calmer."
     end
   end
 end
