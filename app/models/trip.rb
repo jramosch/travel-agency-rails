d25048b40619911add840b38b72d68adefcc7323
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-27 21:41:50 -0500

    created take_trip method

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,11 @@
 class Trip < ActiveRecord::Base
   belongs_to :user
   belongs_to :trip_location, class_name: 'Location', foreign_key: :location_id
+
+  def take_trip
+    
+    if current_user.travel_credits < self.location.price .energy < self.location.energy_rating && current_user.fun
+
+    end
+  end
 end
