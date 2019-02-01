c4e7c0ca1e2d905e4568cbbdb8706ab668204e27
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-02-01 10:45:05 -0500

    created show route and set location instance variable

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -2,4 +2,8 @@ class Admin::LocationsController < ApplicationController
   def index
     @locations = current_user.locations
   end
+
+  def show
+    @location = Location.find(params[:id])
+  end
 end
