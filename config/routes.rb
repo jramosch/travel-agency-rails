5bbcda7709759f22f85bd8083d2bf99cfceed315
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-02-04 23:27:12 -0500

    removed admin user show route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :locations, only: [:show, :index, :edit]
-    resources :users, only: [:show, :index, :edit]
+    resources :users, only: [:index, :edit]
   end
 end
