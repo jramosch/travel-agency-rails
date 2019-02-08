860fad37a32340e3e486d745d24a3909638f4560
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-02-08 11:18:52 -0500

    created average rating instance method

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,12 @@
 class Location < ActiveRecord::Base
   belongs_to :user
   has_many :trips
+
+  def average_rating
+    if self.trips.empty?
+      "Not Enough Ratings"
+    else
+      self.trips.average(:rating).round
+    end
+  end
 end
