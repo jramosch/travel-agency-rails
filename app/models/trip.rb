f89f5942fe66ad56ccd232f1c4f187447e54dc19
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-02-08 03:34:29 -0500

    created update_energy method

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,16 +4,27 @@ class Trip < ActiveRecord::Base
 
   def take_trip
     location = Location.find(self.location_id)
-    user = User.find(self.user_id)
+    user = self.user
     if user.travel_credits < location.price && user.energy < location.activity_rating && user.fun < location.excitement_rating
       "Sorry, you don't have enough credits and maybe you'd like something more relaxing & calmer."
-    elsif user.energy < location.activity_rating && user.fun < location.excitement_rating
+    elsif user.energy <= location.activity_rating && user.fun < location.excitement_rating
       "Sorry, maybe you want something a little more relaxing & calmer."
-    elsif user.fun < location.excitement_rating
+    elsif user.fun <= location.excitement_rating
       "Sorry, maybe you want something a little calmer."
     else
       user.update(:travel_credits => (user.travel_credits -= location.price))
+      update_energy
       "What a trip! Hope you had fun. Don't forget to review."
+    end
+  end
+
+  def update_energy
+    location = Location.find(self.location_id)
+    user = self.user
+    if location.activity_rating == 1
+      user.increment!(:energy)
+    else
+      user.update(:energy => user.energy -= location.activity_rating)
     end
   end
 end
