eb2f21463109d46b5c1e95cf2526b4d7bed2c2f8
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-02-09 09:52:46 -0500

    updated with columns for google authorization and removed email

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -2,13 +2,14 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
-      t.string :uid
       t.string :password_digest
       t.integer :travel_credits
       t.integer :energy
       t.integer :fun
       t.boolean :admin, default: false, null: false
+      t.string :uid
+      t.string :oauth_token
+      t.datetime :oauth_expires_at
     end
   end
 end
