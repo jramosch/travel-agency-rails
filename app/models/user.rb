3fbfa032ad9d53a45f55e3f674eb0698637583dd
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-02-09 13:03:12 -0500

    created class method to create user from google authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  has_secure_password :validations => false
   attr_accessor :trip_rating
 
   has_many :locations
@@ -8,5 +8,16 @@ class User < ActiveRecord::Base
 
   def last_trip
     self.trips.last
+  end
+
+  def self.from_omniuth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
   end
 end
