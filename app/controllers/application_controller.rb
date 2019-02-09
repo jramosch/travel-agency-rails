b1c4ac4064fceff7d6c3407f98254fa8a8316154
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-02-09 16:50:54 -0500

    added before_action filter with require_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login, except: [:home]
   protect_from_forgery with: :exception
   helper_method :current_user, :in_session?
 
@@ -9,11 +10,17 @@ class ApplicationController < ActionController::Base
     end
   end
 
+private
+
   def in_session?
     !!session[:user_id]
   end
 
   def current_user
     @user = User.find(session[:user_id]) unless session[:user_id].nil?
+  end
+
+  def require_login
+    return head(:forbidden) unless in_session?
   end
 end
