386cf8dcc72ec07c7e04f2a925e7ad44437502a7
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-02-12 10:57:26 -0500

    created index route method

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,9 @@
 class TripsController < ApplicationController
   def index
-    
+    if params[:user_id]
+      @trips = User.find(params[:user_id]).trips
+    else
+      @trips. Trip.all
+    end
   end
 end
