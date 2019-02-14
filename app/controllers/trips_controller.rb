e3683117439c06a708919326e74c4fb82a59ff0d
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-02-14 10:32:27 -0500

    updated new method, created create method and trip_params

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -9,5 +9,18 @@ class TripsController < ApplicationController
 
   def new
     @trip = Trip.new
+    @location_options = Location.all.map {|l| ["#{l.name} - $#{l.price}", l.id]}
+  end
+
+  def create
+    location = Location.find(trip_params[:location_id])
+    trip = Trip.create(user_id: current_user.id, location_id: location.id)
+    flash[:notice] = trip.take_trip
+    redirect_to '/'
+  end
+
+  private
+  def trip_params
+    params.require(:trip).permit(:user_id, :location_id)
   end
 end
